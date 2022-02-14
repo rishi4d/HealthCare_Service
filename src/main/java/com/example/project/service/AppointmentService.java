0e377a3902f624cb33192454ddf5741b337e2ebf
User <user.push@hackerrank.workspace>
2022-02-14 14:48:16 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/java/com/example/project/service/AppointmentService.java
+++ src/main/java/com/example/project/service/AppointmentService.java
@@ -38,7 +38,7 @@ public class AppointmentService {
         appointmentRepository.deleteById(appointment);
     }
 
-    public static List<Appointment> findByPatientId(String appointment) {
+    public List<Appointment> findByPatientId(String appointment) {
         return appointmentRepository.findByPatientId(appointment);
     }
   
